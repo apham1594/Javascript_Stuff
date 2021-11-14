2e21710e5378e77f46633cf3a7b1f8cbbc7395f8
apham1594 <88216187+apham1594@users.noreply.github.com>
2021-11-14 01:06:59 -0700

    Made changes

---
+++ app.js
@@ -0,0 +1,89 @@
+// Introduction to Javascript //
+var nameFirst="Anthony";
+var pizzaCount="2";
+var isHungry= true;
+var totalCost= pizzaCount * 12;
+
+console.log(nameFirst);
+console.log(pizzaCount);
+console.log(isHungry);
+console.log(totalCost);
+
+alert("Picking up for "+ "" + nameFirst);
+alert("The pizza cost "+ pizzaCount + " dollars.");
+alert(nameFirst + " your total is "+ totalCost);
+prompt("Are you hungry?");
+
+
+//JS Variables
+//Calculate my Age
+var birthYear = 1998;
+var futureYear  = 2040;
+var age  = futureYear - birthYear;
+console.log('I will be either ' + age + ' or ' + (age - 1));
+
+//Convert F to C
+var celsius = 60;
+var celsiusInF = (celsius*9)/5 + 32;
+console.log(celsius + '°C is ' + celsiusInF + '°F');
+var fahrenheit = 15;
+var fahrenheitInC = ((fahrenheit - 32)*5)/9;
+console.log(fahrenheit + '°F is ' + fahrenheitInC + '°C');
+
+//Function
+function tellFortune(jobTitle, geographicLocation, partner, children) {
+    var future = " You will be a "+ jobTitle + " in " +
+    geographicLocation+ " and married to " + partner + " with " + children + " kids ";
+    document.getElementById("demo").innerHTML=future;
+    console.log(future)
+}
+
+    tellFortune('prostitute', 'Tijuana', 'Carmen Electra', 5);
+    tellFortune('mafia boss', 'Pretoria', 'Lana Rhodes', 1);
+    tellFortune('hitman', 'Tokyo', 'Mia Khalifa', 3);
+
+
+// Array //
+//Create an Array called "donuts" that list 7-10 different items
+var donuts=['glazed', 'jelly', 'chocolate', 'lime zest', 'lemon pie','sprinkled', 'toffee','cherry']
+//1 Using a loop, iterate through this array and console.log all the donuts
+//2 Write the command to remove the first donut fron the array
+//3 Write the command to remove the last donuts from the array
+//4 Write the command to add a new donut "lime zest" to the front of the array
+//5 Write the command to add another donut "lemon pie" to the end of the array
+//6 Use either the join() or toString() method to convert an array to a string.
+//7 Write the command to make a copy of the array using slice. The copy should NOT include two donuts.
+//8 Write the command that gives the indexOf where "chocolate" is located.
+//9 Write the command that gives the indexOf where "glazed" is located (You may get a negative number).
+//10 **Using the splice method, remove one donut from the array and add another.
+//11 **Create a new variable called withSyed and set it equal to the people array concatenated with the string of "Bob".
+//12 **Create a function called getdonuts() that will display all the donuts in your array and display on your page.
+
+
+//The Recipe Card
+// Never forget another recipe!
+// Create an object to hold information on your favorite recipe. It should have properties for title (a string), servings (a number), and ingredients (an array of strings).
+// On separate lines (one console.log statement for each), log the recipe information so it looks like:
+// Mole
+// Serves: 2
+// Ingredients:
+// cinnamon
+// cumin
+// cocoa*//
+
+const recipe = {
+    title: "Mole",
+    serving: 2,
+    ingrediants: ['cumin', 'cinnamon', 'cocoa']
+};
+
+document.write.getElementById("Ingrediants").innerHTML=""
+document.write.getElementById("serving").innerHTML
+document.write.getElementById('title').innerHTML
+
+for (var=i=0;i<recipe.ingrediants.length; i++)
+console.log($recipe.ingrediants)
+
+
+
+
